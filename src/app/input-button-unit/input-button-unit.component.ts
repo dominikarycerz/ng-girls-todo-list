e95b6e6b6c851a0ba2e23ca9e246a4d49e7975ac
Dominika Rycerz <dominikarycerz@hotmail.com>
2022-01-22 16:05:29 +0100

    Add even listener on input and button

--- src/app/input-button-unit/input-button-unit.component.ts
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -2,20 +2,29 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-input-button-unit',
-  template: ` <p>input-button-unit works!</p>
-    The title is: {{ title }}`,
+  template: `
+    <input
+      [value]="title"
+      (click)="clearInput()"
+      (keyup.enter)="changeTitle($event.target.value)"
+    />
+    <button (click)="changeTitle('Button Clicked!')">Save</button>
+    <h2>{{ title }}</h2>
+  `,
   styleUrls: ['./input-button-unit.component.scss'],
 })
 export class InputButtonUnitComponent implements OnInit {
   title = 'Hello world';
 
-  constructor() {
-    this.changeTitle('My first Angular App');
-  }
+  constructor() {}
 
   ngOnInit(): void {}
 
-  changeTitle(newTitle: string) {
+  changeTitle(newTitle: string): void {
     this.title = newTitle;
+  }
+
+  clearInput() {
+    this.title = '';
   }
 }
