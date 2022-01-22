9a17c23557a3ec33c1d3a99b1a4faf63d43484f6
Dominika Rycerz <dominikarycerz@hotmail.com>
2022-01-22 22:08:34 +0100

    Add list item component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,20 @@ import { Component } from '@angular/core';
   template: `
     <h1>{{ title }}</h1>
     <app-input-button-unit></app-input-button-unit>
+    <li *ngFor="let todoItem of todoList">
+      <app-todo-item [item]="todoItem"></app-todo-item>
+    </li>
   `,
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'My Todos';
+  todoList = [
+    { title: 'install NodeJS' },
+    { title: 'install Angular CLI' },
+    { title: 'create new app' },
+    { title: 'serve app' },
+    { title: 'develop app' },
+    { title: 'deploy app' },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { InputButtonUnitComponent } from './input-button-unit/input-button-unit.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
 
 @NgModule({
-  declarations: [AppComponent, InputButtonUnitComponent],
+  declarations: [AppComponent, InputButtonUnitComponent, TodoItemComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
